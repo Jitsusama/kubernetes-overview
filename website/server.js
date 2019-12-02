d78ad141c103cd044fdd7b60b5bf56798855d1e4
Joel Gerber <Joel.Gerber@corp.eastlink.ca>
2019-12-02 07:53:25 -0500

    Use nginx instead of expressjs web server to serve website

--- website/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-app.use(express.static('dist'));
-app.get('/healthy', (request, response) => response.status(204).end());
-
-const listeningPort = process.env['LISTENING_PORT'] || 8080;
-app.listen(listeningPort, () =>
-    console.info(`server listening on port http://0.0.0.0:${listeningPort}`));
